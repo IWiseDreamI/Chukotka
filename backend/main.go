c50067d2ad9ddf04f47b26c7d27f6757782fe5a9
root <root@WiseDream.aeza.network>
2025-02-01 09:39:35 +0000

    Backend update

--- backend/main.go
+++ backend/main.go
@@ -7,7 +7,6 @@ import (
 	"chukotka/routes"
 	"chukotka/core/db"
 	"chukotka/core/config"
-	"chukotka/middlewares"
 )
 
 func main() {
@@ -15,12 +14,24 @@ func main() {
 	db.InitDatabase()
 
 	r := gin.Default()
-	r.Use(middlewares.CORSMiddleware())
+
+	r.Use(func(c *gin.Context) {
+	    c.Writer.Header().Set("Content-Encoding", "identity")
+	    c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
+	    c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
+	    c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+	    c.Writer.Header().Set("Access-Control-Max-Age", "86400") // Cache preflight response
+	    if c.Request.Method == "OPTIONS" {
+	        c.AbortWithStatus(204) // Respond with empty 204 for preflight requests
+	        return
+	   }
+    	   c.Next()
+	})
 
 	routes.RegisterRoutes(r)
 
 	port := config.GetEnv("PORT", "8080")
-	if err := r.Run(":" + port); err != nil {
+	if err := r.Run("0.0.0.0:" + port); err != nil {
 		log.Fatalf("Failed to start server: %v", err)
 	}
 }
