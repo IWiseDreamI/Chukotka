88fa97f36a22f0c5a5195c5a440db7c8eaab438c
root <root@WiseDream.aeza.network>
2025-02-01 07:42:21 +0000

    Updates

--- frontend/src/helpers/form-fields.ts
+++ frontend/src/helpers/form-fields.ts
@@ -26,7 +26,7 @@ const villageForm: FieldInterface[] = [
   { name: "people", field: "markdown", label: "Люди" },
   { name: "culture", field: "markdown", label: "Культура" },
   { name: "socials", field: "markdown", label: "Соцсети" },
-  { name: "districtId", field: "textarea", label: "Район" },
+  { name: "districtId", field: "input", label: "Район" },
   { name: "coordinates", field: "textarea", label: "Координаты" },
 ];
 
