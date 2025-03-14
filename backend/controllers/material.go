67a84d181cb9f252081c7d2ecbf608dd0b3c6b28
Винокуров Камил Джамаледдинович <vinokurov_kd@albank.ru>
2025-03-14 12:35:10 +0900

    Fixes

--- backend/controllers/material.go
+++ backend/controllers/material.go
@@ -1,7 +1,7 @@
 package controllers
 
 import (
-	"chukotka/repository"
+	"chukotka/repositories"
 	"chukotka/models"
 	"net/http"
 	"strconv"
--- backend/repositories/material.go
+++ backend/repositories/material.go
@@ -1,7 +1,7 @@
 package repository
 
 import (
-	"chukotka/db"
+	"chukotka/core/db"
 	"chukotka/models"
 	"errors"
 )
